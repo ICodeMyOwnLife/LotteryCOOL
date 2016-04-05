ed9d3f23ec568597cc76fc85991afa818209a81d
CuteITGuy <grisetroseciel@gmail.com>
2016-04-05 10:50:23 +0700

    Add libraries & scripts

---
+++ LotteryCOOL/Scripts/lottery.js
@@ -0,0 +1,60 @@
+﻿/// <reference path="moment.js" />
+var regex = /(\S+): (\d+(?: - \d+)*)/g;
+
+
+while (m = regex.exec(input)) {
+    console.log(m[1], m[2]);
+}
+
+
+function toProperCase(text) {
+    return text.replace(/w\S*/g, function(s) {
+        return s.charAt(0).toLocaleUpperCase() + s.substring(1).toLocaleLowerCase();
+    });
+}
+
+function getLocation(title) {
+    var loc = /KẾT QUẢ XỔ SỐ (\S+) NGÀY/.exec(title)[2];
+    return toProperCase(loc);
+}
+
+var parseDate = function(link) {
+    var dateString = /\/([^\.]+)\.html/.exec(link)[2];
+    return moment(dateString, "d-M-yyyy");
+};
+
+var parseResults = function(results) {
+    var regexResults = /(\S+): (\d+(?: - \d+)*)/g;
+    var result = {};
+
+    var match;
+    while ((match = regexResults.exec(results))) {
+        result[match[1]] = p
+    }
+
+    return result;
+};
+
+
+var parseLocPrizes = function(locPrizes) {
+    var regexLocResults = /\[(\S+)\] ([^\[]+)/g;
+    var result = {};
+
+    var match;
+    while ((match = regexLocResults.exec(locPrizes))) {
+        result[match[1]] = parseResults(match[2]);
+    }
+
+    return result;
+};
+
+
+function parseRssItem(lotteryRssItem) {
+    var content = lotteryRssItem.description;
+    if (!/\[\S+\]/.test(content)) content = "[" + getLocation(lotteryRssItem.title) + "] " + content;
+
+    return {
+        date: parseDate(lotteryRssItem.link),
+        results: parseLocPrizes(content)
+    };
+}
---
+++ LotteryCOOL/Scripts/main.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="jquery-2.2.2.js" />
+var model = {
+    items: []
+};
+
+var lotteryApp = angular.module("lotteryApp", []);
+var lotteryController = lotteryApp.controller("lotteryController", function($scope) {
+    $scope.model = model;
+});
+
+$(function () {
+    var url = "http://xskt.com.vn/rss-feed/mien-nam-xsmn.rss";
+
+    var yqlUrl = [
+        "http://query.yahooapis.com/v1/public/yql",
+        "?q=" + encodeURIComponent("select * from rss where url='" + url + "'"),
+        "&format=json&callback=?"
+    ].join("");
+
+    var getYqlUrl = function() {
+        var url = $("#txtUrl").val();
+        return [
+        "http://query.yahooapis.com/v1/public/yql",
+        "?q=" + encodeURIComponent("select * from rss where url='" + url + "'"),
+        "&format=json&callback=?"
+        ].join("");
+    };
+
+    $("#cmdGetFeed").click(function () {
+        $.getJSON(getYqlUrl(), function(data) {
+            /*var content = data.results[0];
+            $("#txtRss").text(content);*/
+            model.items = data.query.results.item;
+        });
+    });
+});
