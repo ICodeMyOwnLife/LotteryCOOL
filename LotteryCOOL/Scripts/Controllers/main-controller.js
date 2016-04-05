3a8690810244d98767abec44c5c9073d52d2a3c7
CuteITGuy <grisetroseciel@gmail.com>
2016-04-05 13:10:34 +0700

    Refactor

--- LotteryCOOL/Scripts/Controllers/main-controller.js
+++ LotteryCOOL/Scripts/Controllers/main-controller.js
@@ -1,15 +1,24 @@
 ﻿/// <reference path="angular.js" />
+/// <reference path="../Models/yql.js" />
 /// <reference path="jquery-2.2.2.js" />
+
 var model = {
     items: []
 };
 
 var lotteryApp = angular.module("lotteryApp", []);
-var lotteryController = lotteryApp.controller("lotteryController", function($scope) {
+var mainController = lotteryApp.controller("mainController", function($scope) {
     $scope.model = model;
+
+    $scope.fetchFeed = function() {
+        $.getJSON(Yql.getYqlUrl($scope.url), function(data) {
+            model.items = data.query.results.item;
+            $scope.$apply();
+        });
+    };
 });
 
-$(function () {
+/*$(function() {
     var url = "http://xskt.com.vn/rss-feed/mien-nam-xsmn.rss";
 
     var yqlUrl = [
@@ -21,17 +30,17 @@ $(function () {
     var getYqlUrl = function() {
         var url = $("#txtUrl").val();
         return [
-        "http://query.yahooapis.com/v1/public/yql",
-        "?q=" + encodeURIComponent("select * from rss where url='" + url + "'"),
-        "&format=json&callback=?"
+            "http://query.yahooapis.com/v1/public/yql",
+            "?q=" + encodeURIComponent("select * from rss where url='" + url + "'"),
+            "&format=json&callback=?"
         ].join("");
     };
 
-    $("#cmdGetFeed").click(function () {
+    $("#cmdGetFeed").click(function() {
         $.getJSON(getYqlUrl(), function(data) {
             /*var content = data.results[0];
-            $("#txtRss").text(content);*/
+            $("#txtRss").text(content);#1#
             model.items = data.query.results.item;
         });
     });
-});
+});*/
---
+++ LotteryCOOL/Scripts/Models/yql.js
@@ -0,0 +1,9 @@
+﻿var Yql = {
+    getYqlUrl: function(sourceUrl) {
+        return [
+            "http://query.yahooapis.com/v1/public/yql",
+            "?q=" + encodeURIComponent("select * from rss where url='" + sourceUrl + "'"),
+            "&format=json&callback=?"
+        ].join("");
+    }
+};
